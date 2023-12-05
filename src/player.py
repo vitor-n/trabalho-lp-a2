428e34d3418ab26b7282c826fdc555384b4c5429
pedrotokar <pedrotokar2004@gmail.com>
2023-12-05 20:10:42 -0300

    Add player class. In the moment, it only implements movimentation. It is done by world space, and another class will be in charge of screen positioning.

---
+++ src/player.py
@@ -0,0 +1,51 @@
+import pygame as pg
+from utils import load_image
+
+class Entity(pg.sprite.Sprite):
+    def __init__(self, image_path, initial_position):
+        super().__init__()
+        self.image = load_image(image_path)
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(*initial_position)
+        self.direction = pg.math.Vector2()
+
+class Player(Entity):
+    def __init__(self, image_path, initial_position, map_size):
+        super().__init__(image_path, initial_position)
+        self.speed = 10
+        self.map_size = map_size
+
+    def update(self):
+        self.get_input()
+        if self.direction.magnitude_squared() != 0:
+           self.move(self.speed)
+
+    def get_input(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_UP] and not keys[pg.K_DOWN]:
+            self.direction.y = -1
+        elif keys[pg.K_DOWN] and not keys[pg.K_UP]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+        if keys[pg.K_RIGHT] and not keys[pg.K_LEFT]:
+            self.direction.x = 1
+        elif keys[pg.K_LEFT] and not keys[pg.K_RIGHT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        self.direction = self.direction.normalize()
+        self.rect.x += self.direction.x * speed
+        if self.rect.x > self.map_size[0]:
+            self.rect.x -= self.map_size[0]
+        elif self.rect.x < 0:
+            self.rect.x += self.map_size[0]
+
+        self.rect.y += self.direction.y * speed
+        if self.rect.y > self.map_size[1]:
+            self.rect.y -= self.map_size[1]
+        elif self.rect.y < 0:
+            self.rect.y += self.map_size[1]
