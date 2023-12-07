f973a94e4f0091ce32ed0ab0439a764577a05b33
vito0182 <vitor.mnw@gmail.com>
2023-12-06 23:04:57 -0300

    add gun rotation

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
 from pygame.locals import *
 import pygame as pg
-from settings import SCREEN_DIMENSIONS, TILE_SIZE, FPS
+from settings import SCREEN_DIMENSIONS, TILE_SIZE, FPS, PX_SCALE
 from player import Player
 from utils import load_image, load_map
 from map_ import Map
 import sys
 from camera import Camera
+from weapons import Gun
+from cursor import Cursor
 
-pg.init
+pg.init()
+pg.mouse.set_visible(False)
 clock = pg.time.Clock()
 pg.init()
 screen = pg.display.set_mode(SCREEN_DIMENSIONS)
 clock = pg.time.Clock()
 map_layout = load_map('../trabalho-lp-a2/maps/map.json')["tiles"]
 map = Map(map_layout)
-player = Player(("..", "trabalho-lp-a2", "Sprites", "Player", "player.png"), (TILE_SIZE* 9.5, TILE_SIZE*5.5), map.dimensions)
+player = Player(("..", "trabalho-lp-a2", "Sprites", "Player", "player.png"), (0,0), map.dimensions)
 camera = Camera(screen, map, player)
-
+cursor = Cursor(("..", "trabalho-lp-a2", "Sprites", "cursors", "cursor1.png"), 3, (TILE_SIZE* 9.5, TILE_SIZE*5.5), player)
+gun = Gun(("..", "trabalho-lp-a2", "Sprites", "weapons", "player_weapons", "math_gun.png"), (SCREEN_DIMENSIONS[0]//2,SCREEN_DIMENSIONS[1]//2), cursor)
 
 while True:
     for event in pg.event.get():
@@ -28,8 +32,13 @@ while True:
 
     player.update()
     camera.update()
+    gun.update()
     camera.prepare_map_tiles()
     camera.render()
+    cursor.update()
+
+    screen.blit(gun.image, gun.rect)
+    screen.blit(cursor.image, cursor.rect)
 
     pg.display.update()
     clock.tick(FPS)
--- src/weapons.py
+++ src/weapons.py
@@ -1,23 +1,50 @@
 import pygame
+from settings import PX_SCALE
 from utils import load_image
+import math
 
 class Weapon(pygame.sprite.Sprite):
-    def __init__(self, image_path, position):
+    def __init__(self, image_path, position, cursor):
         super().__init__()
-        self.image = load_image(image_path)
+        self.cursor = cursor
+        self.image = load_image(image_path, PX_SCALE)
+        self.orig_image = self.image
         self.rect = self.image.get_rect()
         self.rect.center = position
 
+    def rotate(self):
+        
+        """if self.recoil:
+            angle_d += math.cos(5 * self.recoil_timer) * 20
+        """
+        self.image = pygame.transform.rotate(self.orig_image, self.cursor.angle_degrees)
+
+        #self.rect = self.image.get_rect(center=(math.cos(self.cursor.angle_radians),
+        #                                        math.sin(self.cursor.angle_radians)))
+
+        #if -angle_d >= 90 or -angle_d <= -90:
+        #    if self.facing_r:
+        #        self.orig_image = pygame.transform.flip(
+        #            self.orig_image, False, True)
+        #        player.image = pygame.transform.flip(player.image, True, False)
+        #        self.facing_r = False
+        #
+        #elif not self.facing_r:
+        #    self.orig_image = pygame.transform.flip(
+        #        self.orig_image, False, True)
+        #    player.image = pygame.transform.flip(player.image, True, False)
+        #    self.facing_r = True
+
 class Gun(Weapon):
-    def __init__(self, image_path, position):
-        super().__init__(image_path, position)
+    def __init__(self, image_path, position, cursor):
+        super().__init__(image_path, position, cursor)
         self.bullets = 10
 
     def shoot(self):
         ...
 
     def update(self):
-        ...
+        self.rotate()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, image_path, position, speed=1):
