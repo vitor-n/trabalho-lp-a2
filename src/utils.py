2cee3c6805ed32211e1eec1e0c172054d7af788e
vito0182 <vitor.mnw@gmail.com>
2023-12-06 21:09:17 -0300

    add map loader function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import json
 from os.path import join
 from settings import TILE_SIZE
 
@@ -7,3 +8,8 @@ def load_image(image_path):
     image = image.convert_alpha()
     image = pg.transform.scale(image, (TILE_SIZE, TILE_SIZE))
     return image
+
+def load_map(filepath):
+    with open(filepath, 'r') as file:
+        map_layout = json.load(file)
+    return map_layout
