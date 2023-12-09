646f3613c2594642adc6548b39f51d8d1612bd20
vito0182 <vitor.mnw@gmail.com>
2023-12-09 12:45:32 -0300

    add draw text module

--- src/utils.py
+++ src/utils.py
@@ -46,3 +46,9 @@ def image_clip(surface, x, y, x_size, y_size):
     image_copy.set_clip(clip)
     image = surface.subsurface(image_copy.get_clip())
     return image.copy()
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_obj, text_rect)
