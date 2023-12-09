518e3ca3d3e0b3da27ec2a4f307cbbf0257e9709
vito0182 <vitor.mnw@gmail.com>
2023-12-09 12:29:50 -0300

    created menu class, changed base menu code

--- src/menu.py
+++ src/menu.py
@@ -1,104 +1,91 @@
 import pygame 
-from settings import FPS, FONT, SCREEN_DIMENSIONS
-import sys
+from settings import FONT, SCREEN_DIMENSIONS
+from utils import load_image, draw_text
 
-pygame.init()
+class Menu:
+    def __init__(self, screen) -> None:
+        self.screen = screen
 
-# Set up the display
-screen = pygame.display.set_mode((SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1]))
-pygame.display.set_caption("Guerreiros Integrais")
-clock = pygame.time.Clock()
+        # Load game menu images
+        self.game_title_image = load_image(("Sprites","Menu","title.png"), 1)  
+        self.game_title_rect = self.game_title_image.get_rect(center=(SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 4)) 
 
-# Load game title image
-game_title_image = pygame.image.load(r"Sprites/Menu/title.png")  
-game_title_rect = game_title_image.get_rect() 
+        self.play_button_image = load_image(("Sprites","Menu","play_button.png"), 1)  
+        self.config_button_image = load_image(("Sprites","Menu","config_button.png"), 1)
 
-# Load button images
-play_button_image = pygame.image.load(r"Sprites/Menu/play_button.png")  
-config_button_image = pygame.image.load(r"Sprites/Menu/config_button.png")  
-
-config_button_rect = config_button_image.get_rect(topleft=(SCREEN_DIMENSIONS[0] // 2 - config_button_image.get_width() // 2, SCREEN_DIMENSIONS[1] // 2 + 100))
-play_button_rect = play_button_image.get_rect(topleft=(SCREEN_DIMENSIONS[0] // 2 - play_button_image.get_width() // 2, SCREEN_DIMENSIONS[1] // 2 + 10))
-
-# Function to display text on the screen
-def draw_text(text, font, color, surface, x, y):
-    text_obj = font.render(text, True, color)
-    text_rect = text_obj.get_rect()
-    text_rect.center = (x, y)
-    surface.blit(text_obj, text_rect)
-
-# Function for the main menu screen
-def main_menu():
-    while True:
-        screen.fill((0,0,0))
+        self.play_button_rect = self.play_button_image.get_rect(center=(SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 10))
+        self.config_button_rect = self.config_button_image.get_rect(center=(SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 100))
         
-        # Draw the game title image
-        screen.blit(game_title_image, (SCREEN_DIMENSIONS[0]// 2 - game_title_rect.width // 2, SCREEN_DIMENSIONS[1] // 4 - game_title_rect.height // 2))
-        
-        # Draw button images
-        screen.blit(play_button_image, (SCREEN_DIMENSIONS[0] // 2 - play_button_image.get_width() // 2, SCREEN_DIMENSIONS[1] // 2 + 10))
-        screen.blit(config_button_image, (SCREEN_DIMENSIONS[0] // 2 - config_button_image.get_width() // 2, SCREEN_DIMENSIONS[1] // 2 + 100))
-        
-        # Event handling
+        self.easy_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2, 200, 50)
+        self.normal_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2 + 70, 200, 50)
+        self.hard_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2 + 140, 200, 50)
+
+        self.selected_difficulty = "normal"
+        self.on_titlescreen = True
+        self.on_config_menu = False
+        self.titlescreen_choice = None
+
+        self.__on_menu = True
+
+    @property
+    def on_menu(self):
+        return self.__on_menu
+
+    def handle_quit_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if play_button_rect.collidepoint(mouse_pos):
-                    print('PRINT PLAY')
-                    return "play"
-                elif config_button_rect.collidepoint(mouse_pos):
-                    print("config, sem print")
-                    return "config"
-            
-        pygame.display.flip()
-     
-# Function for the configuration screen
-def config_menu():
-    while True:
-        screen.fill((0,0,0))
-        draw_text("Selecione a dificuldade.", FONT, (255,255,255), screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 4)
+                quit()
 
-        # Difficulty buttons
-        easy_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2, 200, 50)
-        normal_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2 + 70, 200, 50)
-        hard_button = pygame.Rect(SCREEN_DIMENSIONS[0] // 2 - 100, SCREEN_DIMENSIONS[1] // 2 + 140, 200, 50)
+    def handle_titlescreen_events(self, click):
+        if click:
+            mouse_pos = pygame.mouse.get_pos()
+            if self.play_button_rect.collidepoint(mouse_pos):
+                self.titlescreen_choice = "play"
+                self.on_titlescreen = False
+                self.on_config_menu = False
+                self.__on_menu = False
+            elif self.config_button_rect.collidepoint(mouse_pos):
+                self.titlescreen_choice = "config"
+                self.on_titlescreen = False
+                self.on_config_menu = True
 
-        pygame.draw.rect(screen, (255,255,255), easy_button)
-        draw_text("Fácil", FONT, (0,0,0), screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 25)
-        pygame.draw.rect(screen, (255,255,255), normal_button)
-        draw_text("Normal", FONT, (0,0,0), screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 95)
-        pygame.draw.rect(screen, (255,255,255), hard_button)
-        draw_text("Difícil", FONT, (0,0,0), screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 165)
+    def handle_config_menu_events(self, click):
+        if click:
+            mouse_pos = pygame.mouse.get_pos()
+            if self.easy_button.collidepoint(mouse_pos):
+                self.selected_difficulty = "easy"
+                self.on_config_menu = False
+                self.on_titlescreen = True
+            elif self.normal_button.collidepoint(mouse_pos):
+                self.selected_difficulty = "normal"
+                self.on_config_menu = False
+                self.on_titlescreen = True
+            elif self.hard_button.collidepoint(mouse_pos):
+                self.selected_difficulty = "hard"
+                self.on_config_menu = False
+                self.on_titlescreen = True
 
-        # Event handling for difficulty selection
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if easy_button.collidepoint(mouse_pos):
-                    return "easy"
-                elif normal_button.collidepoint(mouse_pos):
-                    return "normal"
-                elif hard_button.collidepoint(mouse_pos):
-                    return "hard"
-
-        pygame.display.flip()
-        clock.tick(FPS)
-        
-# Main function to control flow
-def main():
-    while True:
-        choice = main_menu()
-        
-        if choice == "config":
-            difficulty = config_menu() #mudar constantes de dificuldade
-            
+    def titlescreen(self, click):
+        self.screen.blit(self.game_title_image, self.game_title_rect)
+        self.screen.blit(self.play_button_image, self.play_button_rect)
+        self.screen.blit(self.config_button_image, self.config_button_rect)
     
+        self.handle_titlescreen_events(click)
 
-if __name__ == "__main__":
-    main()
+    def config_menu(self, click):
+        self.screen.fill("black")
+        draw_text("Selecione a dificuldade.", FONT, "white", self.screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 4)
+        pygame.draw.rect(self.screen, "white", self.easy_button)
+        draw_text("Fácil", FONT, "black", self.screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 25)
+        pygame.draw.rect(self.screen, "white", self.normal_button)
+        draw_text("Normal", FONT, "black", self.screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 95)
+        pygame.draw.rect(self.screen, "white", self.hard_button)
+        draw_text("Difícil", FONT, "black", self.screen, SCREEN_DIMENSIONS[0] // 2, SCREEN_DIMENSIONS[1] // 2 + 165)
+        self.handle_config_menu_events(click)
+
+    def update(self, click=False):
+        if self.on_titlescreen:
+            self.titlescreen(click)
+        elif self.on_config_menu:
+            self.config_menu(click)
