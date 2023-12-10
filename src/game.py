0c85c8d132be9a6b8cee65a4306d18af1c34272e
vito0182 <vitor.mnw@gmail.com>
2023-12-10 05:39:12 -0300

    refactor game class code

--- src/game.py
+++ src/game.py
@@ -11,94 +11,92 @@ from cursor import Cursor
 from enemies import Apache, Roman, Samurai, Viking, IntegralGang
 from text import Font
 from settings import ZERO_GUN_STATS, SINE_GUN_STATS, LINE_GUN_STATS
+from random import randint
 
 
 class Game:
     def __init__(self, screen) -> None:
 
-        self.screen = screen
+        self._screen = screen
 
         map_layout = load_map("maps/map.json")["tiles"]
-        self.map = RepeatMap(map_layout)
+        self._map = RepeatMap(map_layout)
 
-        self.font = Font(("font", "pixel_font_black.png"))
-        self.font2 = Font(("font", "pixel_font_grey.png"))
+        self._font = Font(("font", "pixel_font_black.png"))
+        self._font2 = Font(("font", "pixel_font_grey.png"))
 
-        self.player = Player(("Sprites", "Player", "player.png"), (0,0), Inventory())
-        self.cursor = Cursor(("Sprites", "cursors", "cursor2.png"), (TILE_SIZE* 9.5, TILE_SIZE*5.5))
+        self._player = Player(("Sprites", "Player", "player.png"), (0,0), Inventory())
+        self._cursor = Cursor(("Sprites", "cursors", "cursor2.png"), (TILE_SIZE* 9.5, TILE_SIZE*5.5))
 
-        self.gang = IntegralGang()
-        self.camera = SmoothCamera(screen, self.map, self.player, self.cursor.rect.center)
+        self._gang = IntegralGang()
+        self._camera = SmoothCamera(screen, self._map, self._player, self._cursor.rect.center)
         
-        self.zero_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self.cursor, ZERO_GUN_STATS)
-        self.sine_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self.cursor, SINE_GUN_STATS)
-        self.line_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self.cursor, LINE_GUN_STATS)
+        self._zero_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self._cursor, ZERO_GUN_STATS)
+        self._sine_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self._cursor, SINE_GUN_STATS)
+        self._line_gun = Gun(("Sprites", "weapons", "player_weapons", "math_gun.png"), self._cursor, LINE_GUN_STATS)
 
-        self.player.inventory.add_weapon(self.zero_gun, "0")
-        self.player.inventory.add_weapon(self.sine_gun, "sin(x)")
-        self.player.inventory.add_weapon(self.line_gun, "cx")
+        self._player.inventory.add_weapon(self._zero_gun, "0")
+        self._player.inventory.add_weapon(self._sine_gun, "sin(x)")
+        self._player.inventory.add_weapon(self._line_gun, "cx")
 
         self._delta_time = 0
         self._time_now = 0
         self._last_enemy_spawn_time = 0
 
-        self.bullet_group = pg.sprite.Group()
+        self._bullet_group = pg.sprite.Group()
         
-    @property
-    def on_game(self):
-        return self.__on_game
+        self.menu_time = 0
+        self.on_game = False
 
-    def render_entities(self):
-        self.camera.render_entity(self.player)
+    def _render_entities(self):
+        self._camera.render_entity(self._player)
 
-    def spawn_enemies(self):
-        if self._time_now - self._last_enemy_spawn_time > ENEMY_SPAWN_TIME:
+    def _spawn_enemies(self):
+        if self._time_now - (self._last_enemy_spawn_time+self.menu_time) > ENEMY_SPAWN_TIME:
             self._last_enemy_spawn_time = self._time_now
-            self.gang.random_group(5, 2, 1, self.player.rect)
-            self.gang.set_target_for_all(self.player)
+            self._gang.random_group(randint(2,5), randint(2,3), randint(1,2), self._player.rect)
+            self._gang.set_target_for_all(self._player)
 
-    def update(self):
+    def _update(self):
         self._time_now = pg.time.get_ticks()
-        self.camera.set_cursor_position(self.cursor.rect.center)
-        self.camera.update()
-        self.cursor.update()
-        self.player.update((self.cursor.rect.centerx+self.camera.rect.topleft[0],self.cursor.rect.centery+self.camera.rect.topleft[1]))
-        self.cursor.update()
-        self.gang.update(self._delta_time, self.gang)
-        self.map.expand(self.camera.rect)
+        self._camera.set_cursor_position(self._cursor.rect.center)
+        self._camera.update()
+        self._cursor.update()
+        self._player.update((self._cursor.rect.centerx+self._camera.rect.topleft[0],self._cursor.rect.centery+self._camera.rect.topleft[1]))
+        self._cursor.update()
+        self._gang.update(self._delta_time, self._gang)
+        self._map.expand(self._camera.rect)
 
-    def player_damage_handler(self):
-        for weapon in self.player.inventory.get_weapons():
-            damaged_enemies = pg.sprite.groupcollide(self.gang, weapon.bullet_group, False, True)
+    def _player_damage_handler(self):
+        for weapon in self._player.inventory.get_weapons():
+            damaged_enemies = pg.sprite.groupcollide(self._gang, weapon.bullet_group, False, True)
             for enemy in damaged_enemies:
                 enemy.health - weapon.damage
                 if enemy.health == 0:
                     enemy.kill()
 
-    def enemy_damage_handler(self):
-        for enemy in self.gang:
+    def _enemy_damage_handler(self):
+        for enemy in self._gang:
             if enemy.weapon:
-                if enemy.weapon.rect.colliderect(self.player.rect):
-                    self.player.health - 1
-                elif enemy.rect.colliderect(self.player.rect):
-                    self.player.health - 1
+                if enemy.weapon.rect.colliderect(self._player.rect):
+                    self._player.health - 1
+                elif enemy.rect.colliderect(self._player.rect):
+                    self._player.health - 1
                 if hasattr(enemy.weapon, "bullet_group"):
-                    if pg.sprite.spritecollide(self.player, enemy.weapon.bullet_group, True):
-                        self.player.health - 1
-            self.camera.render_entity(enemy)
+                    if pg.sprite.spritecollide(self._player, enemy.weapon.bullet_group, True):
+                        self._player.health - 1
+            self._camera.render_entity(enemy)
 
-    def render_hud(self):
-        self.screen.blit(self.player.health.bar, (33,30))
-        self.font.render(self.screen, f"sobreviveu: {self._time_now//1000}s", (33,90))
-        self.screen.blit(self.cursor.image, self.cursor.rect)
-
+    def _render_hud(self):
+        self._screen.blit(self._player.health.bar, (33,30))
+        self._font.render(self._screen, f"sobreviveu: {(self._time_now-self.menu_time)//1000}s", (33,90))
+        print(self._time_now-self.menu_time)
     def run(self, delta_time):
         self._delta_time = delta_time
-        self.camera.render_map()
-        self.update()
-        self.spawn_enemies()
-        self.player_damage_handler()
-        self.enemy_damage_handler()
-        self.render_entities()
-        self.render_hud()
-
+        self._camera.render_map()
+        self._update()
+        self._spawn_enemies()
+        self._player_damage_handler()
+        self._enemy_damage_handler()
+        self._render_entities()
+        self._render_hud()
--- src/tempmain.py
+++ src/tempmain.py
@@ -8,7 +8,7 @@ from settings import HIT_SOUND
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
-pygame.mixer.music.set_volume(0)
+pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.load("sounds/ironmain.mp3")
 pygame.mixer.music.play(-1)
 
@@ -20,7 +20,7 @@ pygame.mouse.set_visible(False)
 menu = Menu(screen)
 game = Game(screen)
 
-cursor = Cursor(("Sprites", "cursors", "cursor2.png"), (0,0))
+cursor = Cursor(("Sprites", "cursors", "cursor2.png"), (SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1]))
 
 while True:
 
@@ -39,8 +39,13 @@ while True:
             if event.button == 1:
                 click = True
         menu.update(click)
-    else:
+    elif not game.on_game:
+        game.on_game = True
+        game.menu_time = pygame.time.get_ticks()
+    
+    if game.on_game:
         game.run(delta_time)
+
 
     screen.blit(cursor.image, cursor.rect)
 
